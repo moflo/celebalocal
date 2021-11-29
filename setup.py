26f5bbfcff7d190003d3c5b0ea9bced1bd6eb01f
moflo <github@moflo.me>
2021-11-29 13:34:12 -0800

    Update setup.py structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='celebalocal',
+    version='0.0.1',
+    description='TFDS Manual Loading of Celeb A dataset',
+    url='git@github.com/moflo/celebalocal',
+    author='MoFLo',
+    author_email='github@moflo.me',
+    license='unlicense',
+    packages=['celebalocal'],
+    zip_safe=False
+)
